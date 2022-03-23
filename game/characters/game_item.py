551e864ff3c37e31301f89e969770a1337d677b5
yamiriv1311 <riv21019@byui.edu>
2022-03-22 20:41:55 -0600

    Creation of methods

---
+++ game/characters/game_item.py
@@ -0,0 +1,5 @@
+
+passing_over_item = ""
+
+# This method will be using the concept of polymorphism because we will use it in this file and in the coins.py file too
+# as a base method that will be making the same thing in each class, but can be edited to different results.
---
+++ game/scripting/movement.py
@@ -0,0 +1,5 @@
+
+movement = None
+
+# This method will be using the concept of polymorphism because we will use it in the pacmanMovement.py and phantomsMovement.py file too
+# as a base method that will be making the same thing in each class, but can be edited to different results.
