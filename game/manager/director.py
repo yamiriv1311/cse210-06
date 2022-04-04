b8dcdebedb0519ffeea54624ac2c21c94f380056
samdevbeltran <samigamedev96@gmail.com>
2022-04-03 20:11:25 -0500

    error fixed

--- game/manager/director.py
+++ game/manager/director.py
@@ -1,5 +1,5 @@
-from asyncio import constants
-from tkinter.tix import CELL
+""" from asyncio import constants
+from tkinter.tix import CELL """
 from constants import *
 from game.manager.scene_manager import SceneManager
 from game.characters.charaterStorage import CharacterStorage
@@ -20,7 +20,7 @@ class Director:
         self._video_service.open_window()
         while self._video_service.is_playing():
             self._video_service.start_drawing()
-            self._script.get_actions(INPUT)[0].execute()
+            #self._script.get_actions(INPUT)[0].execute()
             self._script.get_actions(UPDATE)[0].execute()
             self._script.get_actions(OUTPUT)[0].execute()
             self._video_service.stop_drawing()
